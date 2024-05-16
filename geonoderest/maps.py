cbf1c7179b93d555633e2e444c4bf4eb70f0da65
mwallschlaeger <marcel.wallschlaeger@zalf.de>
2024-05-16 09:38:26 +0200

    [Fixes #56] Bug: maps create does not unpack dict before return

--- geonoderest/maps.py
+++ geonoderest/maps.py
@@ -94,7 +94,4 @@ class GeonodeMapsHandler(GeonodeResourceHandler):
             endpoint=self.ENDPOINT_NAME,
             params=json_content,
         )
-        return  r[self.SINGULAR_RESOURCE_NAME]
-
-       
-       
+        return r[self.SINGULAR_RESOURCE_NAME]
