a1497b033b1a990e1f0352cc76e80066b7eca48a
mwallschlaeger <marcel.wallschlaeger@zalf.de>
2024-11-01 15:32:21 +0100

    [Fixes #70] Bug: creating a map via the GeoNode API leads to a map which is not visable on the web UI

--- geonoderest/maps.py
+++ geonoderest/maps.py
@@ -92,11 +92,12 @@ class GeonodeMapsHandler(GeonodeResourceHandler):
 
         # add maplayers to map.blob
         gnResourceHandler = GeonodeResourceHandler(self.gn_credentials)
-        for maplayer_pk in maplayers:
-            try:
-                blob["map"]["layers"].append(gnResourceHandler.get(pk=maplayer_pk))
-            except Exception as err:
-                logging.error(f"dataset {maplayer_pk} not found ...")
+        if maplayers is not None:
+          for maplayer_pk in maplayers:
+              try:
+                  blob["map"]["layers"].append(gnResourceHandler.get(pk=maplayer_pk))
+              except Exception as err:
+                  logging.error(f"dataset {maplayer_pk} not found ...")
 
         base_json_content = {
             "ressource_type": self.SINGULAR_RESOURCE_NAME,
