94f1d7d6b5fae525d146373647194ee24ce1884f
Marcel Wallschlaeger <mwarcel.wallschlaeger@zalf.de>
2024-11-10 23:02:09 +0100

    [Fixes #55] Feature: support delete of multiple objects

--- src/geonoderest/geonodectl.py
+++ src/geonoderest/geonodectl.py
@@ -152,7 +152,7 @@ To use this tool you have to set the following environment variables before star
 
     # DELETE
     resource_delete = resource_subparsers.add_parser("delete", help="delete resource")
-    resource_delete.add_argument(type=str, dest="pk", help="pk of resource to delete")
+    resource_delete.add_argument(type=str, dest="pk", help="pk of resource(s) to delete (range \'1-5\',list \'1,2,3,4,5\', single \'1\') ...")
 
     # METADATA
     resource_metadata = resource_subparsers.add_parser(
@@ -268,7 +268,7 @@ To use this tool you have to set the following environment variables before star
         "delete", help="delete existing datasets"
     )
     datasets_delete.add_argument(
-        type=str, dest="pk", help="pk of dataset to delete ..."
+        type=str, dest="pk", help="pk of dataset(s) to delete (range \'1-5\',list \'1,2,3,4,5\', single \'1\') ..."
     )
 
     #############################
@@ -347,7 +347,7 @@ To use this tool you have to set the following environment variables before star
         "delete", help="delete existing document"
     )
     documents_delete.add_argument(
-        type=str, dest="pk", help="pk of document to delete ..."
+        type=str, dest="pk", help="pk of document(s) to delete (range \'1-5\',list \'1,2,3,4,5\', single \'1\')..."
     )
 
     ########################
@@ -391,7 +391,7 @@ To use this tool you have to set the following environment variables before star
 
     # DELETE
     maps_delete = maps_subparsers.add_parser("delete", help="delete existing map")
-    maps_delete.add_argument(type=str, dest="pk", help="pk of map to delete ...")
+    maps_delete.add_argument(type=str, dest="pk", help="pk of map(s) to delete (range \'1-5\',list \'1,2,3,4,5\', single \'1\') ...")
 
     # CREATE
     maps_create = maps_subparsers.add_parser("create", help="create an (empty) map")
@@ -480,7 +480,7 @@ To use this tool you have to set the following environment variables before star
     geoapps_delete = geoapps_subparsers.add_parser(
         "delete", help="delete existing geoapp"
     )
-    geoapps_delete.add_argument(type=str, dest="pk", help="pk of geoapp to delete ...")
+    geoapps_delete.add_argument(type=str, dest="pk", help="pk of geoapp(s) to delete (range \'1-5\',list \'1,2,3,4,5\', single \'1\') ...")
 
     ##########################
     # USERS ARGUMENT PARSING #
@@ -544,7 +544,7 @@ To use this tool you have to set the following environment variables before star
     )
     # DELETE
     users_delete = users_subparsers.add_parser("delete", help="delete existing user")
-    users_delete.add_argument(type=str, dest="pk", help="pk of geoapp to delete ...")
+    users_delete.add_argument(type=str, dest="pk", help="pk of user(s) to delete (range \'1-5\',list \'1,2,3,4,5\', single \'1\') ...")
 
     # CREATE
     users_create = users_subparsers.add_parser("create", help="create a new user")
